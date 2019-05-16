39be1783ea0bc01c092022f57c0fa1d2b643729c
LiuShii <1164818081@qq.com>
2019-05-16 22:03:57 +0800

    创建学生类，包含学号、姓名、综合测评总分。使用TreeSet保存学生对象，按测评总分排序。如果分数相同，按姓名排序。 最后再用迭代器输出学生对象。

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,47 @@
-public class Student {
+import java.awt.*;
+import java.util.*;
 
+public class Student implements Comparable{
+    private int Sno;
+    private String Name;
+    private int Score;
+    public Student(int Sno,String Name,int Score){
+        this.Sno = Sno;
+        this.Name = Name;
+        this.Score = Score;
+
+    }
+    public int tiqu(Student student){
+        char[] a = student.Name.toCharArray();
+        int b = a[0];
+        return b;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        // TODO Auto-generated method stub
+        Student student = (Student)o;
+
+        return this.Score>student.Score?1:(this.Score==student.Score?(this.tiqu(student)>student.tiqu(student)?1:0):0);
+
+    }
+    public static void print(Set<Student> set){
+        Iterator<Student> it = set.iterator();
+        while (it.hasNext()){
+            Student student = it.next();
+            System.out.println(student.Sno+" "+student.Name+" "+student.Score);
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<Student> set = new TreeSet<>();
+        Student s1 = new Student(001,"Sam",100);
+        Student s2 = new Student(002,"Ader",50);
+        Student s3 = new Student(003,"Stever",66);
+        set.add(s1);
+        set.add(s2);
+        set.add(s3);
+        print(set);
+    }
 
 }
