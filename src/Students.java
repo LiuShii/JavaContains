16fe3af2a4c35cde938a0e6576383308573f55c6
LiuShii <1164818081@qq.com>
2019-05-18 18:33:32 +0800

    2.每一个学生都有对应的归属地。??学生Student，地址String。??学生属性：姓名，年龄。?注意：姓名和年龄相同的视为同一个学生。?保证学生的唯一性。定义map容器，将学生作为键，地址作为值存入map。分别使用keySet与entrySet遍历map中元素

---
+++ src/Students.java
@@ -0,0 +1,52 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Students {
+    private String Name;
+    private int Age;
+    public Students(String Name,int Age){
+        this.Age = Age;
+        this.Name = Name;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Students students = (Students) obj;
+
+        if(this.Name.equals(students.Name)&&this.Age==students.Age){
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        result = (Name==null?0:Name.hashCode());
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Map<Students ,String> map = new HashMap() ;
+        map.put(new Students("wang",18),"HongKong");
+        map.put(new Students("liu",18),"HongKong");
+        map.put(new Students("wang",18),"American");
+        map.put(new Students("wang",21),"zhongguo");
+
+        for(Students students : map.keySet()){
+
+            System.out.println(students.Name+" "+students.Age);
+        }
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()){
+
+            Map.Entry<Students,String> entry = (Map.Entry<Students,String>)it.next();
+            System.out.println(entry.getKey().Name+" "+entry.getKey().Age+" "+entry.getValue());
+        }
+
+
+    }
+}
