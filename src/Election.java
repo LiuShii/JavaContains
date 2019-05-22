09417f2826d93897c01a328ad10edf0c2e3113b6
LiuShii <1164818081@qq.com>
2019-05-22 20:33:43 +0800

    3.写一个统计投票程序。设有3个候选人，有10人参加选举，每个选举人输入两个候选人的编号，要求输出各个候选人的得票数。 输入描述：输入10个选举人提交的候选人编号 输出描述：输出候选人编号及对应的得票。* 要求，用map来表示投票结果。

--- src/Election.java
+++ src/Election.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 
 public class Election {
     private String Candidate[];
-    private String cand1=null;
-    private String cand2=null;
+
 
 
     public Election(String[] Candidate) {
@@ -14,42 +13,56 @@ public class Election {
 
     }
 
-    public String  Sysin(){
+    public String[]  Sysin(){
         Scanner scanner = new Scanner(System.in);
-        String s= scanner.next();
+        String[] s = scanner.nextLine().split(" ");
         return s;
     }
-    public String[] Spilt(String str){
-        String[] strings = str.split(" ");
-        return strings;
-    }
+//    public String[] Spilt(String str){
+//        String[] strings = str.split(" ");
+//        return strings;
+//    }
 
     public static void main(String[] args) {
         Map<String,Integer> map = new HashMap<>();
-        String[] strings = {"Tom","Batman","Jerry"};
-        Election e=new Election(strings);
+        System.out.print("Please put Candidates(only three candidate):");
+        Scanner sc =new Scanner(System.in);
+        String[] Candidates = sc.nextLine().split(" ");
+        while(Candidates.length!=3){
+            System.out.println("put worry!put again!");
+            Scanner sc1 =new Scanner(System.in);
+            Candidates = sc1.nextLine().split(" ");
+            if(Candidates.length==3){
+                break;
+            }
+        }
+        Election e=new Election(Candidates);
 
-        int[] getv=new int[2];
+
+        System.out.println("Please vote to Candidates with two from them!");
+
+        int[] getv=new int[3];
         String[] cand;
-        for(int People = 0;People<=10;People++){
-            cand=e.Spilt(e.Sysin());
-            for(int i=0;i<=3;i++){
-                if(cand[i].equals("Tom")){
+        for(int People = 0;People<10;People++){
+            cand=e.Sysin().clone();
+
+            for(int i=0;i<=1;i++){
+                if(cand[i].equals(Candidates[0])){
                     getv[0]++;
                 }
-                if(cand[i].equals("Batman")){
+                if(cand[i].equals(Candidates[1])){
                     getv[1]++;
                 }
-                if(cand[i].equals("Jerry")){
+                if(cand[i].equals(Candidates[2])){
                     getv[2]++;
                 }
 
             }
 
         }
-        map.put("Tom",getv[0]);
-        map.put("Batman",getv[1]);
-        map.put("Jerry",getv[2]);
+        map.put(Candidates[0],getv[0]);
+        map.put(Candidates[1],getv[1]);
+        map.put(Candidates[2],getv[2]);
 
 
         Iterator it = map.entrySet().iterator();
